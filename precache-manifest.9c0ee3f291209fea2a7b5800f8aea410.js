7fba616a84b3f2a1a3111960291c4514281422ff
anmol224 <anmolj124@gmail.com>
2020-10-01 20:08:23 +0530

    Updates

---
+++ precache-manifest.9c0ee3f291209fea2a7b5800f8aea410.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e6181205c558e10a289",
+    "url": "/anmol224/SpaceX-Clone/static/css/main.28393933.chunk.css"
+  },
+  {
+    "revision": "6e6181205c558e10a289",
+    "url": "/anmol224/SpaceX-Clone/static/js/main.1efd73a2.chunk.js"
+  },
+  {
+    "revision": "22aedac321a6bd2e7846",
+    "url": "/anmol224/SpaceX-Clone/static/js/runtime~main.305bc735.js"
+  },
+  {
+    "revision": "1cffd1e35eb79554867e",
+    "url": "/anmol224/SpaceX-Clone/static/css/2.a5fc85e6.chunk.css"
+  },
+  {
+    "revision": "1cffd1e35eb79554867e",
+    "url": "/anmol224/SpaceX-Clone/static/js/2.b46839a3.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/anmol224/SpaceX-Clone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/anmol224/SpaceX-Clone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/anmol224/SpaceX-Clone/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/anmol224/SpaceX-Clone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/anmol224/SpaceX-Clone/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4a200171ec87e9816e10a731ec5cd667",
+    "url": "/anmol224/SpaceX-Clone/index.html"
+  }
+];
