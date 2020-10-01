6605dab61e3ca65b7668bc848af12b65c646f3f6
anmol224 <anmolj124@gmail.com>
2020-10-01 20:53:54 +0530

    Updates

---
+++ precache-manifest.feed66b57ac8b08b0e05d6e1a77aabc3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0262d2eb10cc93f6d838",
+    "url": "/SpaceX-Clone/static/css/main.6e2cca7c.chunk.css"
+  },
+  {
+    "revision": "0262d2eb10cc93f6d838",
+    "url": "/SpaceX-Clone/static/js/main.0e0824d6.chunk.js"
+  },
+  {
+    "revision": "fa80f98ca7d3a98d37a4",
+    "url": "/SpaceX-Clone/static/js/runtime~main.ca2a79e8.js"
+  },
+  {
+    "revision": "a5d06165dbb8da70b239",
+    "url": "/SpaceX-Clone/static/css/2.b495f568.chunk.css"
+  },
+  {
+    "revision": "a5d06165dbb8da70b239",
+    "url": "/SpaceX-Clone/static/js/2.b46839a3.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/SpaceX-Clone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/SpaceX-Clone/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/SpaceX-Clone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/SpaceX-Clone/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/SpaceX-Clone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "d6ab3d046f5dc6024c9ee464fc596d24",
+    "url": "/SpaceX-Clone/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/anmol224/SpaceX-Clone.git/precache-manifest.6a09c2742ae53a7ad39bc1522d44aa7a.js"
+  "/SpaceX-Clone/precache-manifest.feed66b57ac8b08b0e05d6e1a77aabc3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/anmol224/SpaceX-Clone.git/index.html", {
+workbox.routing.registerNavigationRoute("/SpaceX-Clone/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
