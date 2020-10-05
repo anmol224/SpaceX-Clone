679034ede1fb7abe33bbce26e39ad0fd17e5e16c
anmol224 <anmolj124@gmail.com>
2020-10-05 20:44:00 +0530

    'spacex'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 
 import "./App.css";
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 import Main from './Components/MainComponent'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
       <React.Fragment>
       <Main></Main>
        
       </React.Fragment>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
